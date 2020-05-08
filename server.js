4a642bd3d3115503979724fd8b917519592905b7
Clare <cuicuiaaa0307@gmail.com>
2020-05-08 19:00:51 +1000

    add promisify

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require("path");
 const util = require("util");
 
 const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 app.use(express.static('public'));
 
@@ -22,37 +23,37 @@ app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 })
 
-app.get("/api/notes", function(req, res) {
-    readFileAsync(path.join(__dirname, "db/db.json"), "utf8")
-    .then(notes => {
-        console.log(notes);
-        res.json(JSON.parse(notes));
-    })
+app.get("/api/notes", async function(req, res) {
+    let string = await readFileAsync("db/db.json", "utf8");
+
+    res.json(JSON.parse(string));
+    
     
 });
 
-app.post("/api/notes", function(req, res) {
-    readFileAsync("db/db.json", "utf8", function(string) {
-        let notes = JSON.parse(string);
+app.post("/api/notes", async function(req, res) {
+    let string = await readFileAsync("db/db.json", "utf8");
 
-        let newNote = req.body;
-        let id = v4();
-        newNote.id = id;
-        
-        notes.push(newNote);
+    let notes = JSON.parse(string);
 
-        let data = JSON.stringify(notes);
+    let newNote = req.body;
+    let id = v4();
+    newNote.id = id;
+    
+    notes.push(newNote);
 
-        writeFileAsync("db/db.json", data, function() {
-            if(err) throw err;
-            console.log("success!");
-        });
-        
-        res.send({
-            status: 200,
-            message: 'success!'
-        });
-    })
+    let data = JSON.stringify(notes);
+
+    writeFileAsync("db/db.json", data, function(err) {
+        if(err) throw err;
+        console.log("success!");
+    });
+
+    res.send({
+        status: 200,
+        message: 'success!'
+    });
+    
     
     
 });
